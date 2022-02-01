7e97b9faa8d85ece94f20c7461a2280e2be551fa
EfraimCancio <efraimfonseca@gmail.com>
2022-02-01 16:52:12 -0300

    Create CardGame component

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const CardGame = () => {
+    return `
+        <article class="card-game">
+           <img src="images/alura-pixel.png" alt="logo da alura"/>
+        </article>
+    `;
+}
+
+export default CardGame;
